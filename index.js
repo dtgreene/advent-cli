161aa67ea9e2b302e56ddeadcaaf417d0671b773
dtgreene <dylan.greene@shipt.com>
2023-12-01 14:11:07 -0600

    Add save state

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 import inquirer from 'inquirer';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { existsSync, readFileSync } from 'node:fs';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 import 'colors';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
-const dayChoices = Array.from(Array(31)).map((_, index) => String(index + 1));
+const savePath = path.resolve(__dirname, 'save.dat');
+const dayChoices = Array.from(Array(31)).map((_, index) => index + 1);
 
-let prevDay = null;
-let prevPart = null;
+async function prompt() {
+  if (existsSync(savePath)) {
+    const { choice } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: [
+          { name: 'Repeat the previous selection', value: 0 },
+          { name: 'Select a day to solve', value: 1 },
+        ],
+      },
+    ]);
 
-async function main() {
-  while (true) {
-    if (prevDay !== null && prevPart !== null) {
-      const { choice } = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'choice',
-          message: 'What would you like to do?',
-          choices: [
-            { name: 'Repeat the previous selection', value: 1 },
-            { name: 'Select a day to solve', value: 0 },
-          ],
-        },
-      ]);
+    if (choice === 0) {
+      try {
+        const { prevDay, prevPart } = JSON.parse(
+          readFileSync(savePath, 'utf-8')
+        );
 
-      if (choice === 0) {
-        await promptDay();
-      } else {
-        await solveDay(prevDay, prevPart);
+        if (prevDay !== undefined && prevPart !== undefined) {
+          return solveDay(prevDay, prevPart);
+        } else {
+          throw new Error('Missing prevDay or prevPart props');
+        }
+      } catch (error) {
+        console.warn(`Could not load save data: ${error.message}`.yellow);
+        return;
       }
-    } else {
-      await promptDay();
     }
-
-    console.log('\n');
   }
+
+  return promptDay();
 }
 
 async function promptDay() {
@@ -58,8 +63,8 @@ async function promptDay() {
         name: 'part',
         message: 'Which part should be solved?',
         choices: [
-          { name: 'Part 1', value: 0 },
-          { name: 'Part 2', value: 1 },
+          { name: 'Part 1', value: 1 },
+          { name: 'Part 2', value: 2 },
         ],
       },
     ]);
@@ -71,9 +76,6 @@ async function promptDay() {
 }
 
 async function solveDay(day, part) {
-  prevDay = day;
-  prevPart = part;
-
   const solutionPath = path.resolve(__dirname, `./solutions/${day}.js`);
   const inputPath = path.resolve(__dirname, `./solutions/${day}.txt`);
 
@@ -81,7 +83,7 @@ async function solveDay(day, part) {
     const input = readFileSync(inputPath, 'utf-8').trim();
     const { partOne, partTwo } = await import(solutionPath);
 
-    if ((part === 0 && !partOne) || (part === 1 && !partTwo)) {
+    if ((part === 1 && !partOne) || (part === 2 && !partTwo)) {
       console.warn(
         `Part ${part} does not exist in the day ${day} solution.`.yellow
       );
@@ -89,16 +91,14 @@ async function solveDay(day, part) {
       try {
         let answer = null;
 
-        if (part === 0) {
+        if (part === 1) {
           answer = partOne(input);
         } else {
           answer = partTwo(input);
         }
 
         if (answer !== null) {
-          console.log(
-            `Answer for day ${day} part ${part + 1}: ${answer}`.green
-          );
+          console.log(`Answer for day ${day} part ${part}: ${answer}`.green);
         } else {
           console.log(
             `Answer for day ${day} part ${
@@ -113,7 +113,13 @@ async function solveDay(day, part) {
   } else {
     console.warn(`Input or solution missing for day ${day}`.yellow);
   }
+
+  try {
+    writeFileSync(savePath, JSON.stringify({ prevDay: day, prevPart: part }));
+  } catch (error) {
+    console.warn(`Could not save data: ${error.message}`.yellow);
+  }
 }
 
 console.clear();
-main();
+await prompt();
